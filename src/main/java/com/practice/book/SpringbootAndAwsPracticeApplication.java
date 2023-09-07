f54229278282d2684c7dc46ca4bd61d8711c08a8
jskim <js.story14@gmail.com>
2023-09-07 15:03:35 +0900

    Initial commit

---
+++ src/main/java/com/practice/book/SpringbootAndAwsPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.practice.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAndAwsPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAndAwsPracticeApplication.class, args);
+    }
+
+}
