d84e8d1ebb3d32148e0e110e76b733768b383f56
jskim <js.story14@gmail.com>
2023-09-08 11:53:49 +0900

    03-3.3
    - JPA 적용
    - 테스트코드 작성

---
+++ src/main/java/com/practice/book/domain/posts/Posts.java
@@ -0,0 +1,36 @@
+package com.practice.book.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@Entity // jpa 어노테이션, 주요 어노테이션일수록 클래스와 가깝게 둔다
+public class Posts {
+    @Id //pk 필드
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder // 생성자에 선언하면 생성자에 포함된 필드만 빌더에 포함된다.
+    public Posts(Long id, String title, String content, String author) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
