11ceb3bfbb4eb0333af05daa464643469a319236
jskim <js.story14@gmail.com>
2023-09-24 17:25:53 +0900

    feat(login) : security login 페이지 개선
    - 기본 로그인 페이지가 아닌 alert창 활성화되고, 메인페이지로 이동하도록 변경

--- src/main/java/com/practice/book/config/auth/SecurityConfig.java
+++ src/main/java/com/practice/book/config/auth/SecurityConfig.java
@@ -19,16 +19,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable()
                 .and()
                 .authorizeRequests()
-                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile", "/login").permitAll()
                 .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                 .anyRequest().authenticated()
                 .and()
                 .logout()
                 .logoutSuccessUrl("/")
                 .and()
-                .oauth2Login()
+                .oauth2Login().loginPage("/login")
                 .userInfoEndpoint()
-                .userService(customOAuth2UserService);
+                .userService(customOAuth2UserService)
+        ;
     }
 
 }
