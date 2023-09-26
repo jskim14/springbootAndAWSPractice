9195be61dcb758ee001a1825900d21051b6551f3
jskim <js.story14@gmail.com>
2023-09-27 02:11:48 +0900

    feat : NoSuchElementException 추가
    - exception handler에 NoSuchElementException 추가

--- src/main/java/com/practice/book/core/exception/CommonExceptionHandler.java
+++ src/main/java/com/practice/book/core/exception/CommonExceptionHandler.java
@@ -10,6 +10,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.validation.ConstraintViolationException;
+import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class CommonExceptionHandler extends ResponseEntityExceptionHandler {
@@ -19,5 +20,15 @@ public class CommonExceptionHandler extends ResponseEntityExceptionHandler {
         ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.BAD_REQUEST, exception.getConstraintViolations());
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(CommonResponse.fail(errorResponse));
+    }
+
+    @ExceptionHandler({NoSuchElementException.class})
+    public final ResponseEntity<CommonResponse<?>> noSuchElementHandler(RuntimeException exception, WebRequest request){
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .httpStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+                .message(exception.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(CommonResponse.fail(errorResponse));
     }
 }
